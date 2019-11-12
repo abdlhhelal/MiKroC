5ecba469c4db4586635132c219ddb18348b9daa3
Abdallah Helal <48185485+abdlhhelal@users.noreply.github.com>
2019-11-12 06:18:25 +0200

    Update KeyPad_PW.c

--- KeyPad_PW.c
+++ KeyPad_PW.c
@@ -18,40 +18,40 @@ void main(void)
                      TrisB=0b00111000; // PORTB b0..b2 O/P | b3..b6 I/P
 
                      while(1)
-                     {
-
-                      if ( Counter == 4 )    // 4 Digits entered and Wrong Password
                       {
+                      
+                      if ( Counter == 4 )    // 4 Digits entered and Wrong Password
+                       {
                        Counter = 0;
                        PORTA.b1 = 1;     // Turn on Red Green
                        delay_ms(5000);  // 5 sec delay
                        PORTA.b1 = 0;   // Turn off Red Green
-                      }
+                       }
                       PORTB = 1; // R1 = 1, R2 = 0 , R3 = 0
                       if (R1 == 1 && C1 == 1 )
-                      {
+                       {
                        Checker = 0;
                        Counter++;
                        }
                       if (R1 == 1 && C2 == 1 )
-                      {
+                       {
                        Checker = 0;
                        Counter++;
                        }
                        if (R1 == 1 && C3 == 1 )
-                      {
+                       {
                        Checker = 0;
                        Counter++;
                        }
 
                        PORTB=2; // R1 = 0, R2 = 1 , R3 = 0
                       if (R2 == 1 && C1 == 1 )
-                      {
+                       {
                        Checker = 0;
                        Counter++;
                        }
                       if (R2 == 1 && C2 == 1 )
-                      {
+                       {
                        if (Checker == 2)
                        Checker = 3;
                        else
@@ -59,7 +59,7 @@ void main(void)
                        Counter++;
                        }
                        if (R2 == 1 && C3 == 1 )
-                      {
+                       {
                        if ( Checker == 1 )
                        Checker = 2;
                        else
@@ -69,34 +69,31 @@ void main(void)
 
 
                        PORTB=4; // R1 = 0, R2 = 0 , R3 = 1
-                      if (R3 == 1 && C1 == 1 )
-                      {
+                       if (R3 == 1 && C1 == 1 )
+                       {
                        if (Checker == 3)
-                      { Checker = 4; // Password is right
+                       {
+                        Checker = 4; // Password is right
                         PORTA.b0 = 1; // Turn on Green Led
                         delay_ms(10000); // 10 sec delay
                         PORTA.b0 = 0; // Turn off Green Led
-                      }
+                       }
                        else
                        { Checker = 0;
                          Counter++;
                        }
-                       }
-                      if (R3 == 1 && C2 == 1 )
-                      {
-                       Checker = 1;
-
+                       
+                       if (R3 == 1 && C2 == 1 )
+                       {
+                        Checker = 1;
+                       
                        }
                        if (R3 == 1 && C3 == 1 )
-                      {
+                       {
                        Checker = 0;
                        Counter++;
                        }
-
-                      }
-
-
-
-
+                         
+                       }
 
 }
